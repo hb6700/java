d2c1d6e2744cd39cd9ae680f83e02f431b4e9101
hb6700 <hb6700@naver.com>
2023-09-29 19:52:33 +0900

    230929

---
+++ src/com/test/java/Ex24_Method.java
@@ -0,0 +1,43 @@
+//Ex24_Method.java
+package com.test.java;
+
+public class Ex24_Method {
+
+	public static void main(String[] args) {
+		//Swap
+		// - 2개의 공간의 값을 서로 교환하는 작업
+		
+		int a = 10;
+		int b = 20;
+		swap(a,b);
+		
+		String s1 = "홍길동";
+		String s2 = "아무개";
+		swap(s1,s2);
+	}//main
+	
+	public static void swap(int a, int b) {
+		System.out.printf("a: %d, b: %d\n", a, b);
+		
+		int temp;		//빈 컵 생성
+		
+		temp = a;
+		a = b;
+		b = temp;
+		
+		System.out.printf("a: %d, b: %d\n", a, b);
+	}
+	
+	public static void swap(String s1, String s2) {
+		System.out.printf("s1: %s, s2: %s\n", s1, s2);
+		
+		String temp;	//빈 컵 생성
+		
+		temp = s1;
+		s1 = s2;
+		s2 = temp;
+		
+		System.out.printf("s1: %s, s2: %s\n", s1, s2);
+	}
+
+}
